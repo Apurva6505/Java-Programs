8fae14180af8fd63205771fa3e858f1bd78ef5d7
Apurva Pawar <102655654+Apurva6505@users.noreply.github.com>
2022-11-01 01:52:28 +0530

    practise problems

---
+++ LL.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class LL {
+    public static void main(String[] args) {
+        // Make a Linked List & add the following elements to it : (1, 5, 7, 3 , 8, 2,
+        // 3). Search for the number 7 & display its index.
+        // LinkedList<String> list = new LinkedList<String>();
+        // list.add("1");
+        // list.add("5");
+        // list.add("7");
+        // list.add("3");
+        // list.add("8");
+        // list.add("2");
+        // list.add("3");
+        // System.out.println(list);
+
+        // for( int i=0;i<list.size();i++){
+        // if(list.get(i)== "7"){
+        // System.out.println(i);
+        // }
+        // }
+
+    }
+}
