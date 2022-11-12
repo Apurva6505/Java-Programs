73a2fa4e776d5cb83e50868708e3e80f2e1d0e39
Apurva Pawar <102655654+Apurva6505@users.noreply.github.com>
2022-11-12 16:55:05 +0530

    Add files via upload

---
+++ novapattern.java
@@ -0,0 +1,23 @@
+public class novapattern{
+    public static void main(String args[]){
+        int n = 6;
+        char ch = 'A';
+        
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(" ");
+            
+            }
+            for(int j=1;j<i;j++){
+                System.out.print(ch+" ");
+                 ch++;
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+                 ch++;
+            }
+            System.out.println();
+
+        }
+    }
+}
